1ebf5952ec59e0ec2c71dfe0f6e0465b07ae7261
JulianLange <44167720+JulianLange@users.noreply.github.com>
2018-11-23 15:40:45 +0100

    Skript um Gegner erst beim näherkommen aggressiv zu machen

---
+++ zombie_ki.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_ki : MonoBehaviour {
+    public GameObject Player;
+    public float ChaseDistance;
+    Vector2 direction;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(gameObject.transform.position, Player.transform.position) < ChaseDistance)
+        {
+            gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().target = Player.transform;
+            //set target
+        }
+        else
+        {
+            //clear target or maybe not?
+        }
+        
+    }
+}
