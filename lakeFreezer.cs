30991782dc14c17eb219fd3277bb22a899ee3a3d
JulianLange <44167720+JulianLange@users.noreply.github.com>
2018-12-29 12:43:30 +0100

    skript hinzugefügt um See mit Eisschollen zu füllen

---
+++ lakeFreezer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//fills an area with similar game objects
+public class lakeFreezer : MonoBehaviour {
+    public Transform start;
+    public int amount_x;
+    public int amount_z;
+    public GameObject filler;
+    public int x_offset;
+    public int z_offset;
+    
+
+	void Start () {
+		for (int i = 0; i <= amount_x; i++)
+        {
+            for (int j = 0; j<= amount_z; j++)
+            {
+                Instantiate(filler, start.position + new Vector3(i*x_offset, 0, j*z_offset+(i%2)*z_offset/2), filler.transform.rotation);
+            }
+        }
+	}
+	
+
+}
